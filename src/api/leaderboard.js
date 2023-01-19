0610cac78a956ad1b8f9d193e4ef7f3851c1ff29
kappacap <kappacap@proton.me>
2023-01-19 09:28:57 +0000

    leaderboard current and last months

--- src/api/leaderboard.js
+++ src/api/leaderboard.js
@@ -9,19 +9,27 @@ export async function getLeaderboard(params) {
 
 	if (!params) params = {};
 
-	let { start, end } = params;
+	let { previous } = params;
 
 	const now = new Date();
 
-	if (!start) {
-		// timestamp at beginning of this month
-		start = parseInt((new Date(now.getFullYear(), now.getMonth(), 1)).getTime() / 1000);
-	}
-	if (!end) {
-		// timestamp at end of this month
-		const nextMonth = new Date(now.getFullYear(), now.getMonth()+1,1);
-		const endOfMonth = new Date(nextMonth.getTime()-1);
-		end = parseInt(endOfMonth.getTime() / 1000);
+	const lastMonth = new Date(now.getFullYear(), now.getMonth()-1, 1);
+	const startOfLastMonth = new Date(lastMonth.getFullYear(), lastMonth.getMonth(), 1);
+	const startLastMonth = startOfLastMonth.getTime() / 1000;
+
+	const startThisMonth = parseInt((new Date(now.getFullYear(), now.getMonth(), 1)).getTime() / 1000);
+
+	const nextMonth = new Date(now.getFullYear(), now.getMonth()+1,1);
+	const endOfMonth = new Date(nextMonth.getTime()-1);
+	const endThisMonth = parseInt(endOfMonth.getTime() / 1000);
+
+	let start, end;
+	if (previous) {
+		start = startLastMonth;
+		end = startThisMonth;
+	} else {
+		start = startThisMonth;
+		end = endThisMonth;
 	}
 
 	try {
--- src/components/leaderboard/Leaderboard.svelte
+++ src/components/leaderboard/Leaderboard.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { onDestroy } from 'svelte'
+	import { onMount, onDestroy } from 'svelte'
 
 	import { getLeaderboard } from '@api/leaderboard' 
 
@@ -7,22 +7,47 @@
 
 	let isLoading = true, t;
 
-	async function fetchData() {
-		clearTimeout(t);
-		await getLeaderboard();
-		isLoading = false;
-		// t = setTimeout(fetchData, 10 * 60 * 1000);
+	let showPreviousLeaderboard = false;
+
+	async function loadLeaderboard(isPrevious) {
+		if (isPrevious) {
+			showPreviousLeaderboard = true;
+			await getLeaderboard({previous: true});
+		} else {
+			showPreviousLeaderboard = false;
+			await getLeaderboard();
+		}
 	}
 
-	fetchData();
+	onMount(() => {
+		loadLeaderboard();
+	});
 
 	onDestroy(() => {
 		clearTimeout(t);
 	});
 
 	let currentDate = new Date();
+
 	let month = currentDate.toLocaleString('default', { month: 'short' });
 	let year = currentDate.getFullYear();
+
+	const lastMonthDate = new Date(currentDate.getFullYear(), currentDate.getMonth()-1, 1);
+	const lastMonth = lastMonthDate.toLocaleString('default', { month: 'short' });
+	const lastMonthYear = lastMonthDate.getFullYear();
+
+	let currentPrizes = {
+		1: '25000'
+	};
+
+	let previousPrizes = {
+		1: '25000'
+	};
+
+	function getPrize(rank) {
+		if (showPreviousLeaderboard) return previousPrizes[rank];
+		return currentPrizes[rank];
+	}
 
 </script>
 
@@ -32,34 +57,126 @@
 		max-width: 1280px;
 		margin: 0 auto;
 	}
-	.container {
-	  display: flex;
-	  flex-wrap: wrap;
-	  width: 100%;
+	
+	.table {
+		--grid-template: 70px 1fr 1fr 1fr;
 	}
+
+	.header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		margin-bottom: 16px;
+	}
+
+	.title {
+		font-weight: 600;
+		font-size: 24px;
+		padding-bottom: 12px;
+	}
+	.subtitle {
+		color: var(--text300);
+	}
+
+	.buttons a {
+		color: var(--primary);
+	}
+
+	.buttons a.active {
+		color: var(--text0);
+		font-weight: 600;
+		pointer-events: none;
+	}
+
+	.table {
+		border: 1px solid var(--layer100);
+		border-radius: 6px;
+	}
+
+	.table-header {
+		display: grid;
+		align-items: center;
+		height: 38px;
+		border-bottom: 1px solid var(--layer100);
+		grid-template-columns: var(--grid-template);
+		color: var(--text300);
+		font-size: 85%;
+	}
+
 	.row {
-	  display: flex;
-	  width: 100%;
+		display: grid;
+		align-items: center;
+		grid-template-columns: var(--grid-template);
+		border-bottom: 1px solid var(--layer0-hover);
+		height: 50px;
 	}
-	.user-column {
-	  flex: 1;
+
+	.cell {
+		display: flex;
+		align-items: center;
+		text-transform: capitalize;
+		height: 100%;
+		padding: 0 25px;
+		justify-content: flex-end;
+		text-align: right;
 	}
-	.pnl-column {
-	  flex: 1;
+	.cell.la {
+		justify-content: flex-start;
+		text-align: left;
 	}
+	.cell.highlighted {
+		background-color: var(--layer50);
+	}
+
+	.cell img {
+		margin-right: 8px;
+		width: 18px;
+	}
+
+	.grayed {
+		opacity: 0.5;
+		font-size: 80%;
+		display: block;
+	}
+
 </style>
 
 <div class='wrapper'>
 
-	<h3>{month} {year}</h3>
+	<div class='header'>
+		<div class='left'>
+			<div class='title'>
+				{#if showPreviousLeaderboard}
+					{lastMonth} {lastMonthYear}
+				{:else}
+					{month} {year}
+				{/if}
+			</div>
+			<div class='subtitle'>Rank and win prizes monthly based on your trading P/L.</div>
+		</div>
+		<div class='right buttons'>
+			<a class:active={showPreviousLeaderboard == false} on:click={() => {loadLeaderboard()}}>Current Month</a> | 
+			<a class:active={showPreviousLeaderboard == true} on:click={() => {loadLeaderboard(true)}}>Last Month</a>
+		</div>
+	</div>
 
-	<div class="container">
-	  {#each $leaderboard as { user, pnlUsd } }
-	    <div class="row">
-	      <div class="user-column">{user}</div>
-	      <div class="pnl-column">{pnlUsd}</div>
-	    </div>
-	  {/each}
+	<div class='table'>
+		<div class='table-header'>
+			<div class='cell la'>Rank</div>
+			<div class='cell la'>User</div>
+			<div class='cell'>P/L</div>
+			<div class='cell highlighted'>Prize</div>
+		</div>
+		<div class='table-body'>
+			{#each $leaderboard as { user, pnlUsd }, i }
+			<div class='row'>
+				<div class='cell la'>{i+1}</div>
+				<div class='cell la'>{user}</div>
+				<div class='cell'>{pnlUsd}</div>
+				<div class='cell'>{getPrize(i+1)}</div>
+			</div>
+			{/each}
+		</div>
 	</div>
 
 </div>
